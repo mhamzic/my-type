0b1041ab6bafcf3d2c48e3890e8baf38b720d443
mhamzic <mhamzic@gmail.com>
2019-11-09 15:25:55 +0100

    stuck

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 let sentences = [
-  "ten ate neite ate nee enet ite ate inet ent eate",
-  "Too ato too nOt enot one totA not anot tOO aNot",
-  "oat itain oat tain nate eate tea anne inant nean",
-  "itant eate anot eat nato inate eat anot tain eat",
-  "nee ene ate ite tent tiet ent ine ene ete ene ate"
+  "ten ate",
+  "Too ato too",
+  "oat",
+  "itant",
+  "nee ene ate ite tent tiet ent ine ene ete ene ate ete ene ateete ene ateete ene ate"
 ];
+
+// let sentences = [
+//   "ten ate neite ate nee enet ite ate inet",
+//   "Too ato too nOt enot one totA not anot tOO aNot",
+//   "oat itain oat tain nate eate tea anne inant nean inant nean",
+//   "itant eate anot eat nato inate",
+//   "nee ene ate ite tent tiet ent ine ene ete ene ate ete ene ateete ene ateete ene ate"
+// ];
 
 // hide upper case keyboard
 $("#keyboard-upper-container").hide();
@@ -41,22 +49,49 @@ $(document).on("keypress", function(e) {
 });
 
 // show sentences
-$("#sentence").text(sentences[0]);
-var keyPressCount = 0;
+var sentenceNumber = 0;
+$("#sentence").text(sentences[sentenceNumber]);
 var yellowBlockPosition = 18;
-var sentenceCounter = 0;
-$("#sentence").text(sentences[sentenceCounter]);
-let chars = sentences[sentenceCounter].split("");
-$("#target-letter").text(chars[keyPressCount]);
-$("#yellow-block").css("left", `${yellowBlockPosition}px`);
 
-$(document).on("keypress", function(e) {
-  $("#target-letter").text(chars[keyPressCount + 1]);
+var sentence = sentences[keyPressCount];
+var keyPressCount = 0;
+var currentSentence = sentences[0].split("");
+$("#target-letter").text(currentSentence[keyPressCount]);
+
+$(document).keypress(function(e) {
+  var currentSentence = sentences[sentenceNumber].split("");
+  // show next letter to type
+  currentLetter = currentSentence[keyPressCount + 1];
+  $("#target-letter").text(currentLetter);
+  // move to next sentence
+  if (keyPressCount === currentSentence.length) {
+    console.log("first sentence over");
+
+    keyPressCount = 0;
+    sentenceNumber++;
+    yellowBlockPosition = 18;
+
+    var currentSentence = sentences[sentenceNumber].split("");
+    currentLetter = currentSentence[keyPressCount];
+    console.log(currentLetter);
+
+    $("#target-letter").text(currentLetter);
+    $("#sentence").text(sentences[sentenceNumber]);
+    $("#feedback").text("");
+    var s = "";
+  }
+  // highlight letter yellow
   $("#yellow-block").css("left", `${yellowBlockPosition}px`);
+  if (e.keyCode == 32) {
+    yellowBlockPosition += 30;
+  } else {
+    yellowBlockPosition += 18;
+  }
 
+  // check if user entered correct letter
   var s = String.fromCharCode(e.which);
 
-  if (s === chars[keyPressCount]) {
+  if (s === currentSentence[keyPressCount]) {
     $("#feedback").append(
       '<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>'
     );
@@ -69,26 +104,70 @@ $(document).on("keypress", function(e) {
   }
 
   keyPressCount++;
-  yellowBlockPosition += 18;
-
-  if (keyPressCount === chars.length) {
-    sentenceCounter++;
-    keyPressCount = 0;
-  }
-  console.log(chars.length);
-  console.log(sentenceCounter);
-  console.log(keyPressCount);
-
-  // console.log(keyPressCount);
-  // console.log(yellowBlockPosition);
+  console.log("Keypress count: " + keyPressCount);
+  console.log(`Current sentence size ${currentSentence.length}`);
+  console.log(`Current sentence ${sentenceNumber}`);
 });
 
-$;
+// show sentences
+// $("#sentence").text(sentences[0]);
+// var keyPressCount = 0;
+// var yellowBlockPosition = 18;
+// var sentenceCounter = 0;
+// $("#sentence").text(sentences[sentenceCounter]);
+// let chars = sentences[sentenceCounter].split("");
+// $("#target-letter").text(chars[keyPressCount]);
+// $("#yellow-block").css("left", `${yellowBlockPosition}px`);
 
-// keyPressCount++;
+// $(document).on("keypress", function(e) {
+//   $("#target-letter").text(chars[keyPressCount + 1]);
+//   $("#yellow-block").css("left", `${yellowBlockPosition}px`);
 
-// $.each(sentences, function(index, value) {
-//     $.each(value.split(""), function(i, v) {
-//       $("#target-letter").text(v);
-//     });
-//   });
+//   var s = String.fromCharCode(e.which);
+
+//   if (s === chars[keyPressCount]) {
+//     $("#feedback").append(
+//       '<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>'
+//     );
+//     console.log("OK");
+//   } else {
+//     $("#feedback").append(
+//       '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>'
+//     );
+//     console.log("NOT OK");
+//   }
+
+//   keyPressCount++;
+//   yellowBlockPosition += 18;
+
+//   if (keyPressCount === chars.length) {
+//     sentenceCounter++;
+//     keyPressCount = 0;
+//   }
+//   console.log(chars.length);
+//   console.log(sentenceCounter);
+//   console.log(keyPressCount);
+
+//   // console.log(keyPressCount);
+//   // console.log(yellowBlockPosition);
+// });
+
+// $;
+
+// // keyPressCount++;
+
+// // $.each(sentences, function(index, value) {
+// //     $.each(value.split(""), function(i, v) {
+// //       $("#target-letter").text(v);
+// //     });
+// //   });
+
+// if (e.which !== 0) {
+//   t += String.fromCharCode(e.which);
+//   if (sentence.substring(0, t.length) == t) {
+//     $("#sentence").html(
+//       "<span class='highlight'>" + t + "</span>" + sentence.substring(t.length)
+//     );
+//   } else {
+//     t = t.substring(0, t.length - 1);
+//   }
---
+++ newapp.js
@@ -0,0 +1,96 @@
+// KEYBOARD SHOW, HIDE, HIGHLIGHT PRESSED KEY
+
+// hide upper case keyboard
+$("#keyboard-upper-container").hide();
+
+// show upper case keyboard on pressing Shift key
+$(document).keydown(function(e) {
+  if (e.keyCode == 16) {
+    $("#keyboard-upper-container").show();
+    $("#keyboard-lower-container").hide();
+  }
+});
+
+// hide upper case keyboard on releasing shift key
+$(document).keyup(function(e) {
+  if (e.keyCode == 16) {
+    $("#keyboard-upper-container").hide();
+    $("#keyboard-lower-container").show();
+  }
+});
+
+// highlight key on press
+$(document).on("keypress", function(e) {
+  var keyPressed = e.which;
+  $(`#${keyPressed}`).css({
+    backgroundColor: "yellow"
+  });
+  $(document).on("keyup", function() {
+    $(`#${keyPressed}`).css({
+      backgroundColor: "#f5f5f5"
+    });
+  });
+});
+
+// SENTENCES, LETTERS AND YELLOW POINTER
+
+const sentences = [
+  "ten ate",
+  "Too ato too",
+  "oat",
+  "itant",
+  "nee ene ate ite tent tiet ent ine ene ete ene ate ete ene ateete ene ateete ene ate"
+];
+
+// setting starter parameters
+let letterCounter = 0;
+let sentenceCounter = 0;
+let currentSentence = sentences[sentenceCounter];
+let currentLetter = sentences[sentenceCounter][letterCounter];
+
+$("#sentence").text(currentSentence); //set first sentence
+$("#target-letter").text(currentLetter); //set first letter
+
+$(document).keypress(function(e) {
+  // setting parameters after key is pressed
+  let keyPressed = String.fromCharCode(e.which);
+  let currentSentence = sentences[sentenceCounter];
+  let currentLetter = sentences[sentenceCounter][letterCounter];
+  let nextLetter = sentences[sentenceCounter][letterCounter + 1];
+
+  if (nextLetter != undefined) {
+    // as long as there is letters in the sentence
+    $("#sentence").text(currentSentence);
+    $("#target-letter").text(nextLetter);
+    $("#yellow-block").animate({ left: "+=18px" }, 100, "linear");
+
+    // checking for glyphicons to fire
+    if (keyPressed == currentLetter) {
+      addGlyphIconOK();
+    } else {
+      addGlyphIconWrong();
+    }
+    // counting letters;
+    letterCounter++;
+
+    // if there is no more letters, reset parameters and move to next sentence
+  } else {
+    console.log("no more characters");
+    sentenceCounter++;
+    letterCounter = 0;
+    $("#yellow-block").removeAttr("style");
+    $("#feedback").text("");
+  }
+});
+
+function addGlyphIconOK() {
+  $("#feedback").append(
+    '<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>'
+  );
+}
+
+function addGlyphIconWrong() {
+  $("#feedback").append(
+    '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>'
+  );
+}
